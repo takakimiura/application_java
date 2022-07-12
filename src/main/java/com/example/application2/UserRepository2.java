9742bd4f035c670a0264da1a5571b3e77aa0754f
takaki miura <takaki.miura@broadleaf.co.jp>
2022-07-12 10:40:49 +0900

    Idを降順にする機能の追加

--- src/main/java/com/example/application2/UserRepository2.java
+++ src/main/java/com/example/application2/UserRepository2.java
@@ -1,7 +1,17 @@
 package com.example.application2;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository2 extends JpaRepository<UserEntity2, Integer> {
 
+  @Query(
+    value = "SELECT * FROM user ORDER BY id DESC", //←SQL文
+    nativeQuery = true //直接SQLを実行する機能
+  )
+  public List<UserEntity2> findAllOrderByIdDesc(); //idを降順で表示させる
 }
